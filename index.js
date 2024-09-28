f04cf3f5554abfdf462cad86679a0d2a52a5a01f
Samuel_Juarez_Lucas <samueljuarezlucas.90@gmail.com>
2024-09-28 14:02:24 -0600

    added pokemon POST

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const pokemon = require('./routes/pokemon');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+    return res.status(200).send("Bienvenido al Pokedex");
+});
+
+app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running...")
+});
+
+
+
+
+
+
+
+
+
+
 /*
 Verbos HTP
-GET
+GET obtener recursos
 POST GUARDAR DATOS
 PATCH ACTUALIZACIÓN DE 1 DATO
 PUT MODIFICAR TODOS LOS ELEMENTOS
@@ -10,10 +39,11 @@ DELETE ELIMINA UN RECURSO
 */
 //funcion sin nombre
 
-app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido");
-});
-app.listen(3000, () => {
-    console.log("Server is running...")
-});
+    /*
+    for(i = 0; i<pokemon.length; i++){
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase()) {
+            return res.status(200).send(pokemon[i]);
+        }        
+    }
+    return res.status(404).send("Pokemon no encontrado");
+*/

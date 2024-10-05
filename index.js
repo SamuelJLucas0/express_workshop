8f452a4b3026497e14b336e8d56fac13e2757cbe
Samuel_Juarez_Lucas <samueljuarezlucas.90@gmail.com>
2024-10-05 16:39:02 -0600

    Usuarios

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({ code: 1, message: "Bienvenido al Pokédex"});
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) => {
     return res.status(404).json({code: 404, message: "URL no encontrada"});
 });
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running...")
+    console.log("Server is running...");
 });
 
 
